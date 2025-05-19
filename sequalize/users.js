8a8ba87d475df704a8355b93da9bd843bda95273
Reckless312 <ionutcora66@gmail.com>
2025-05-19 13:18:00 +0300

    fix

--- sequalize/users.js
+++ sequalize/users.js
@@ -1,19 +1,15 @@
 const {Sequelize, DataTypes} = require("sequelize");
 const {pg} = require("pg");
 
-const sequelize = new Sequelize("postgres://postgres.uutgjvlxpphpavxsscsw:RiT4MUjw4v2wuPZU@aws-0-us-east-1.pooler.supabase.com:6543/postgres?sslmode=require&supa=base-pooler.x", {
-    database: "postgres",
-    username: "postgres",
-    password: "RiT4MUjw4v2wuPZU",
-    host: "db.uutgjvlxpphpavxsscsw.supabase.co",
+const sequelize = new Sequelize("postgres://postgres.uutgjvlxpphpavxsscsw:RiT4MUjw4v2wuPZU@aws-0-us-east-1.pooler.supabase.com:6543/postgres?sslmode=disable&supa=base-pooler.x", {
     dialect: 'postgres',
     dialectModule: pg,
-    dialectOptions: {
+    /*dialectOptions: {
         ssl: {
             require: true,
             rejectUnauthorized: false // <<<<<< THIS LINE
         }
-    }
+    }*/
 });
 
 const User = sequelize.define('User', {
